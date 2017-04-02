c59c0b4b64813744364b4a714641369cff9e107e
jclcntl <pascal.schlumpf@gmail.com>
2017-04-02 11:06:08 -0700

    Create simplifySwitchCase.groovy

---
+++ simplifySwitchCase.groovy
@@ -0,0 +1,25 @@
+def pew = "hello"
+
+switch (pew) {             
+    case "hello":  
+        printf "pewpew"
+        break;
+    case "hello2":  
+        printf "pewpewpew"
+        break;             
+     default: 
+        monthString = "Invalid month";
+        break;         
+}
+
+def pewpewClosure = {
+    printf "pewpew"
+}
+
+def pewpew2Closure = {
+    printf "pewpew2"
+}
+
+def pew2 = ["hello":pewpewClosure, "hello2": pewpew2Closure]
+        
+pew2["pewpew2"].call()​
